14aed04f29bf8c4d0d91066eb85cd80be0137c47
Justine-Lance-Pabalan <165749021+Justine-Lance-Pabalan@users.noreply.github.com>
2026-04-22 19:33:17 +0800

    Update score.py

--- utils/score.py
+++ utils/score.py
@@ -1,6 +1,6 @@
 class Score:
-	def __init__(self, username, points, stages_won, date):
-		self.username = username
-		self.points = points
-		self.stages_won = stages_won
-		self.date = date
+    def __init__(self, username, points, stages_won, date):
+        self.username = username
+        self.points = points
+        self.stages_won = stages_won
+        self.date = date
